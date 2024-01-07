608e9d53ccdbf4c0147be7848fbb117fbf5e63d2
우수연 <woosean999@gmail.com>
2024-01-07 13:19:00 +0900

    add class test

---
+++ __test__/class.test.ts
@@ -0,0 +1,36 @@
+class Dollar {
+  money: number;
+
+  constructor(num: number) {
+    this.money = num;
+  }
+
+  times(num: number) {
+    this.money *= num;
+  }
+}
+
+
+test("multiplier", () => {
+  const dollar = new Dollar(5);
+  dollar.times(2);
+  expect(dollar.money).toEqual(10);
+});
+
+class Money {
+  money: number;
+  currency: Currency;
+
+  constructor(num: number) {
+    this.money = num;
+  }
+
+  times(num: number) {
+    this.money *= num;
+  }
+}
+
+test("testMultiplication", () => {
+
+
+});
