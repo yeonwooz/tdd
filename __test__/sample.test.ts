d221c7e892a6ea100df607e5d84d4ce47bc9e81d
우수연 <woosean999@gmail.com>
2024-01-07 12:31:13 +0900

    multiplierByCurrency

--- __test__/sample.test.ts
+++ __test__/sample.test.ts
@@ -4,7 +4,7 @@ test("Dummy unit test", () => {
   expect(actual).toBe(3);
 });
 
-const multiplier = (money, num) => {
+const multiplier = (money: number, num: number): number => {
   return money * num;
 };
 
@@ -15,6 +15,48 @@ test("multiplier", () => {
   const result = multiplier(money, num);
 
   // then
-  expect(result).toBe(money * num);
+  expect(result).toEqual(money * num);
 });
 
+enum Currency {
+  USD,
+  CHF,
+}
+
+const getExchangeRate = (currency: Currency) => {
+  switch (currency) {
+    case Currency.USD:
+      return 1;
+    case Currency.CHF:
+      return 2;
+    default:
+      return 1;
+  }
+};
+
+const calcuatePrice = (dollorPrice: number, currency: Currency) => {
+  return dollorPrice * getExchangeRate(currency);
+};
+
+const convertToDollar = (price: number, currency: Currency) => {
+  return Math.floor(price / getExchangeRate(currency));
+};
+
+test("multiplierByCurrency", () => {
+  // given
+
+  // when
+  const money = 5;
+  const usdResult = convertToDollar(
+    calcuatePrice(money, Currency.USD),
+    Currency.USD,
+  );
+  const chfResult = convertToDollar(
+    calcuatePrice(money, Currency.CHF),
+    Currency.CHF,
+  );
+  // then
+
+  expect(chfResult).toEqual(5);
+  expect(chfResult).toEqual(usdResult);
+});
